ba09f29b1e38e0924ff07c3084179ea28ca84bfc
SergioPosadas <sergioposid@gmail.com>
2022-05-26 17:39:18 +0200

    Añadidos request y response, archivos json para el intercambio de información entre sensor y sistema de radiación

--- client.py
+++ client.py
@@ -2,14 +2,6 @@
 
 import socket, sys, time, datetime, logging, json
 
-####################################################################################
-#                                                                                  #
-# OBTENER EL ARCHIVO JSON, LEERLO, CARGAR LOS DATOS EN LAS RESPECTIVAS VARIABLES,  #
-# CODIFICARLO A BYTES Y ENVIARLO. RECIBIR EL JSON DE SOLARMODULE, DECODIFICARLO,   #
-# LEERLO Y MOSTRAR POR PANTALLA EL CONTENIDO.                                      #
-#                                                                                  #
-####################################################################################
-
 
 class Client(object):
     """
@@ -27,21 +19,17 @@ class Client(object):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
         # Obtención del archivo json con el contenido del mensaje que se va a enviar a SolarModule
-        with open("Request.json", "r") as req:
-        	self.request = json.loads(req, sys.stdout)
+        with open("Request.json", "r", encoding='utf-8') as req:
+        	self.request = json.load(req)
         	
         	# (Control) Mostrar por pantalla el archivo obtenido
-        	print(self.request)
+        	logging.info(self.request)
              
         
     def run(self):
         """
             Método para conectar con el módulo y enviar mensajes
         """
-        
-        # Variable msg para enviar información mientras hago que funcione el json
-        #msg = 'Hola'
-        #msg = msg.encode('utf-8')
                 
         # Comando para conectarse con el socket del módulo cogiendo los valores de 
         # los argumentos introducidos al ejecutar el programa
@@ -49,23 +37,25 @@ class Client(object):
                 
         while True:
             # Una vez conectado con el socket del módulo, enviar el mensaje json codificado en bytes
-            self.socket.sendall(self.request)
+            print(str(self.request))
+            self.socket.sendall(bytes(str(self.request), 'utf-8'))
                         
             # Al haber realizado la conexión guardamos la información recibida en la variable data y la decodificamos
             data = self.socket.recv(1024)
-            data = json.load(data)
-            
+                        
             # Si se ha recibido información del socket del módulo, se va mostrando una enumeración
             # cada seguundo hasta que no se envíe nada o se corte la conexión
             if data:            
                 logging.info('Receiving data from SolarModules socket...')
-                print(data)
+                logging.info(data)
                 time.sleep(1)
                 
             # Si no se recibe nada se corta la conexión creada
             else:
                 self.socket.close()
                 break
+        
+
         
 if __name__ == '__main__':
     
--- solar_module.py
+++ solar_module.py
@@ -1,12 +1,5 @@
 import socket, sys, logging, time, datetime, json
 
-####################################################################################
-#                                                                                  #
-# OBTENER EL ARCHIVO JSON, LEERLO, CARGAR LOS DATOS EN LAS RESPECTIVAS VARIABLES,  #
-# CODIFICARLO A BYTES Y ENVIARLO. RECIBIR EL JSON DE SOLARMODULE, DECODIFICARLO,   #
-# LEERLO Y MOSTRAR POR PANTALLA EL CONTENIDO.                                      #
-#                                                                                  #
-####################################################################################
 
 
 class SolarModule(object):
@@ -26,28 +19,24 @@ class SolarModule(object):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       
         
         # Obtención del archivo json con el contenido del mensaje que se va a enviar a SolarModule
-        with open("Response.json", "r") as resp:
-        	self.response = json.loads(resp, sys.stdout)
+        with open("Response.json", "r", encoding='utf-8') as resp:
+        	self.response = json.load(resp)
         	
         	# (Control) Mostrar por pantalla el archivo obtenido
-        	print(self.response)
-        
+        	logging.info(self.response)
+        	        
         
     def run(self):
         """
             Método para poner al socket a escuchar conexiones
         """
-        
-        # Variable msg para enviar información mientras hago que funcione el json
-        #msg = 'Hola'
-        #msg = msg.encode('utf-8') 
                 
         # Comando para indicarle al socket en qué puerto escuchar las conexiones
         self.socket.bind((self.host, self.port))
                 
         # Comando para que el socket acepte conexiones, el 1 indica que sólo se aceptará una conexión
         self.socket.listen(1)
-        
+                       
         while True:
         # Comando para estar a la espera de conexiones, devolviendo la conexión del cliente 
         # (guardada en connect) y la dirección (host y puerto) de dicha conexión 
@@ -61,12 +50,12 @@ class SolarModule(object):
             while True:
                 # Se comprueba si se recibe información del socket cliente
                 data = connect.recv(1024)
-                data = json.load(data)
-                print(data)
+                
+                logging.info(data)
                 
                 # Si se ha recibido información de dicho socket, se responde al cliente a modo de ACK
                 if data:
-                    connect.sendall(self.response)
+                    connect.sendall(bytes(str(self.response), 'utf-8'))
                     
                 # Si no se ha recibido información se corta la conexión con dicho socket
                 else:
